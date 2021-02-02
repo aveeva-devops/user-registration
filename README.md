d3c43defc44daf652e4438dca4ebdda11fa23b68
Pardeep Chahal <pardeepchahal@pardeeps-mbp.myfiosgateway.com>
2021-02-02 00:17:02 -0500

    changes done to insert image in mongo db

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# restful-flask-python3
+# Use below commands if docker-compose setup gives trouble
+
+$ virtualenv -p python3 venv
+$ source venv/bin/activate
+
+(venv)$ pip install Flask-PyMongo
+
+(venv)$ python mongo-1.py
+
+Ref: https://www.youtube.com/watch?v=DsgAuceHha4
---
+++ web/mongo-1.py
@@ -0,0 +1,29 @@
+# mongo.py
+from flask import request, url_for, Flask, jsonify
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
+mongo = PyMongo(app)
+
+# Insert Images in mongoDB
+
+@app.route('/create', methods=['POST'])
+def create():
+  if 'profile_image' in request.files:
+    profile_image = request.files['profile_image']
+    mongo.save_file(profile_image.filename, profile_image)
+    mongo.db.users.insert({'username': request.get('username'), 'profile_image_name': profile_image.filename})
+  return 'Done'
+
+# Endpoint to retrieve data from mongoDB
+
+@app.route('/file/<filename>')
+def file(filename):
+  return mongo.send_file(filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
